ac56b4b8091de46a580ba3fd9d58d3739068bf27
Yunxiang-Li <kitzukikokone@gmail.com>
2022-03-26 05:04:20 -0400

    Refactor UserInfo and UserRecordContainerActivity classes and fix most of warnings

--- app/src/main/java/edu/neu/madcourse/numad22sp_yunxiangli/UserInfo.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_yunxiangli/UserInfo.java
@@ -1,19 +1,25 @@
 package edu.neu.madcourse.numad22sp_yunxiangli;
 
-public class UserInfo {
+/**
+ * This class represents each user's information.
+ */
+public class UserInfo
+{
+
+    // Store the user's name in string form.
     private String username;
 
+    // This is required by ProGuard.
     public UserInfo(){}
 
-    public UserInfo(String username) {
+    // One argument constructor.
+    public UserInfo(String username)
+    {
         this.username = username;
     }
 
-    public String getUsername() {
+    public String getUsername()
+    {
         return this.username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 }
--- app/src/main/java/edu/neu/madcourse/numad22sp_yunxiangli/UserRecordContainerActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_yunxiangli/UserRecordContainerActivity.java
@@ -6,17 +6,23 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class UserRecordContainerActivity extends AppCompatActivity {
+/**
+ * This class represents the activity of the user record container(each row).
+ */
+public class UserRecordContainerActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_record_container);
     }
 
 
     // called upon clicking Send sticker button
-    public void onClickSendStickerButton(View view) {
+    public void onClickSendStickerButton(View view)
+    {
         System.out.println("Send sticker button clicked");
         startActivity(new Intent(this, StickersDisplayActivity.class));
     }
